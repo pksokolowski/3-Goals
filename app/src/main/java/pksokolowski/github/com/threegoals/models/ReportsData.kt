55d45dee79903894ef50ac4b9824b9e3e4832f05
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-07-17 20:58:11 +0200

    let ReportsData load the data on it's init
    When a ReportsData object is constructed, it now fetches reports data
    from the database for the edition it's supposed to cover.

--- app/src/main/java/pksokolowski/github/com/threegoals/models/ReportsData.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/models/ReportsData.kt
@@ -1,4 +1,24 @@
 package pksokolowski.github.com.threegoals.models
 
-class ReportsData(val edition: Edition) {
+import android.content.Context
+import pksokolowski.github.com.threegoals.database.DbHelper
+
+class ReportsData(context: Context, val edition: Edition) {
+    private var goals = mutableListOf<Goal>()
+    private var reports = arrayOfNulls<Report?>(edition.length_in_days)
+
+    init {
+        val db = DbHelper.getInstance(context)
+        goals = db.getGoals(edition)
+        val editionsReports = db.getReports(edition)
+
+        // assign reports to day slots:
+        for (i in editionsReports.indices){
+            val report = editionsReports[i]
+            val day_num = report.day_num
+            reports[day_num] = report
+        }
+    }
+
+
 }
