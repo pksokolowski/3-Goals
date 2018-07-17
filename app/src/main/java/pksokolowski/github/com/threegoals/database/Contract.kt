e14655818fa55c6440c68e3603d82e8214cbb918
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-07-17 12:30:43 +0200

    add SQL for tables creation

--- app/src/main/java/pksokolowski/github/com/threegoals/database/Contract.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/database/Contract.kt
@@ -6,6 +6,7 @@ internal object Contract {
 
     internal class reports : BaseColumns {
         companion object {
+            val ID = "id"
             val TABLE_NAME = "reports"
             val COLUMN_NAME_DAY_NUM = "day_num"
             val COLUMN_NAME_TIME_STAMP = "time_stamp"
@@ -17,16 +18,18 @@ internal object Contract {
 
     internal class goals : BaseColumns {
         companion object {
+            val ID = "id"
             val TABLE_NAME = "goals"
             val COLUMN_NAME_NAME = "name"
             val COLUMN_NAME_INITIAL = "initial"
-            val COLUMN_NAME_INDEX = "index"
+            val COLUMN_NAME_POSITION = "position"
             val COLUMN_NAME_EDITION = "edition"
         }
     }
 
     internal class editions : BaseColumns {
         companion object {
+            val ID = "id"
             val TABLE_NAME = "editions"
             val COLUMN_NAME_TITLE = "title"
             val COLUMN_NAME_GOALS_COUNT = "goals_count"
--- app/src/main/java/pksokolowski/github/com/threegoals/database/DbHelper.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/database/DbHelper.kt
@@ -6,14 +6,40 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class DbHelper private constructor (context: Context) : SQLiteOpenHelper(context, "dataBase.db", null, 1) {
     override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        with(p0!!){
+            execSQL("CREATE TABLE " + Contract.reports.TABLE_NAME + " (" +
+                    Contract.reports.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    Contract.reports.COLUMN_NAME_DAY_NUM + " INTEGER, " +
+                    Contract.reports.COLUMN_NAME_TIME_STAMP + " INTEGER, " +
+                    Contract.reports.COLUMN_NAME_SCORE_TRYING_HARD + " INTEGER, " +
+                    Contract.reports.COLUMN_NAME_SCORE_POSITIVES + " INTEGER, " +
+                    Contract.reports.COLUMN_NAME_GOAL + " INTEGER," +
+                    " FOREIGN KEY (" + Contract.reports.COLUMN_NAME_GOAL + ") REFERENCES " + Contract.goals.TABLE_NAME + "(" + Contract.goals.ID + "));")
+
+            execSQL("CREATE TABLE " + Contract.goals.TABLE_NAME + " (" +
+                    Contract.goals.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    Contract.goals.COLUMN_NAME_NAME + " TEXT, " +
+                    Contract.goals.COLUMN_NAME_INITIAL+ " TEXT, " +
+                    Contract.goals.COLUMN_NAME_POSITION + " INTEGER, " +
+                    Contract.goals.COLUMN_NAME_EDITION + " INTEGER, " +
+                    " FOREIGN KEY (" + Contract.goals.COLUMN_NAME_EDITION + ") REFERENCES " + Contract.editions.TABLE_NAME + "(" + Contract.editions.ID + "));")
+
+            execSQL("CREATE TABLE " + Contract.editions.TABLE_NAME + " (" +
+                    Contract.editions.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    Contract.editions.COLUMN_NAME_TITLE + " TEXT, " +
+                    Contract.editions.COLUMN_NAME_GOALS_COUNT + " INTEGER, " +
+                    Contract.editions.COLUMN_NAME_START_DAY_0_HOUR + " INTEGER, " +
+                    Contract.editions.COLUMN_NAME_EDITION_LENGTH + " INTEGER);")
+        }
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        p0!!.execSQL("DROP TABLE IF EXISTS "+ Contract.reports.TABLE_NAME)
-        p0.execSQL("DROP TABLE IF EXISTS "+ Contract.goals.TABLE_NAME)
-        p0.execSQL("DROP TABLE IF EXISTS "+ Contract.editions.TABLE_NAME)
-        onCreate(p0)
+        with(p0!!){
+            execSQL("DROP TABLE IF EXISTS "+ Contract.reports.TABLE_NAME)
+            execSQL("DROP TABLE IF EXISTS "+ Contract.goals.TABLE_NAME)
+            execSQL("DROP TABLE IF EXISTS "+ Contract.editions.TABLE_NAME)
+            onCreate(this)
+        }
     }
     companion object {
 
@@ -24,8 +50,14 @@ class DbHelper private constructor (context: Context) : SQLiteOpenHelper(context
                     INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
                 }
 
-        private fun buildDatabase(context: Context) = DbHelper(context)
+        private fun buildDatabase(context: Context) : DbHelper{
+            var helper = DbHelper(context)
+            helper.sDataBase = helper.writableDatabase
+            return helper
+        }
 
     }
+
+    private var sDataBase: SQLiteDatabase? = null
 
 }
