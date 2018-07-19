9f39fe05f373817483f38235f56a84a9f828fe71
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-07-19 16:12:23 +0200

    fix compound controls sharing state
    When onSaveInstanceState is called, parcel with View's data is saved
    with the View's ID, and onRestoreInstanceState is called for all views
    with that ID with the data saved. Therefore if multiple instances of a
    View share id (for example because multiple instances of a compound
    control exist within the same layout and each has Views with same IDs as
    every other instance of that compound control) then only the last
    instance's state is persisted and then onRestoreInstanceState is
    provided with that state for every instance: so all of them share the
    state now. In order to fix this, the ReportForm was changed into a
    Fragment.

--- app/src/main/java/pksokolowski/github/com/threegoals/reporter/ReportForm.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/reporter/ReportForm.kt
@@ -1,16 +1,18 @@
 package pksokolowski.github.com.threegoals.reporter
 
-import android.content.Context
-import android.support.constraint.ConstraintLayout
-import android.util.AttributeSet
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
-import kotlinx.android.synthetic.main.report_form.view.*
+import android.view.ViewGroup
 import pksokolowski.github.com.threegoals.R
 
-class ReportForm@JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0) : ConstraintLayout(context, attrs, defStyle) {
-    init {
-        View.inflate(context, R.layout.report_form, this)
-    }
-
+class ReportForm : Fragment() {
     private val romanNums = arrayOf("Ⅰ", "Ⅱ", "Ⅲ")
+    private var mView : View? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mView = inflater.inflate(R.layout.report_form, container)
+        return mView
+    }
 }
