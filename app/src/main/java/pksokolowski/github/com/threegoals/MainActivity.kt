a52596a8b3e3ea5e6f78b389508134e26fa4e142
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-07-22 21:05:59 +0200

    add basic UI elements to MainActivity
    Two ImageViews were added, when clicked they're intended to open
    EditorDialogFragment and a help dialog respectively.
    EditorDialogFragment is already being opened onClick.

--- app/src/main/java/pksokolowski/github/com/threegoals/MainActivity.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/MainActivity.kt
@@ -2,6 +2,8 @@ package pksokolowski.github.com.threegoals
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import pksokolowski.github.com.threegoals.editor.EditorDialogFragment
 import pksokolowski.github.com.threegoals.notifications.NotificationsManager
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         NotificationsManager.createNotificationChannels(this)
         setContentView(R.layout.activity_main)
+
+        // assign UI actions
+        val edition = EditionsManager.getCurrentEdition(this)
+        if(edition != null) {
+            editor_imageview.setOnClickListener { EditorDialogFragment.showDialog(this, edition) }
+        }
     }
 }
