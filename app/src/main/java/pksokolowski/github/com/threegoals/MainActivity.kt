fc27e5e1244ad271bf7fe9eb56de702565c0ab17
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2018-07-24 17:10:00 +0200

    make Reporter and Main Activities full screen
    Both of the Activities are now full screen in immersive mode. This
    enables greater control over the overall visual experience of the user
    and provides much more space for the UI.

--- app/src/main/java/pksokolowski/github/com/threegoals/MainActivity.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/MainActivity.kt
@@ -2,6 +2,7 @@ package pksokolowski.github.com.threegoals
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import pksokolowski.github.com.threegoals.editor.EditorDialogFragment
 import pksokolowski.github.com.threegoals.notifications.NotificationsManager
@@ -17,6 +18,18 @@ class MainActivity : AppCompatActivity() {
         val edition = EditionsManager.getCurrentEdition(this)
         if(edition != null) {
             editor_imageview.setOnClickListener { EditorDialogFragment.showDialog(this, edition) }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         }
     }
 }
--- app/src/main/java/pksokolowski/github/com/threegoals/reporter/ReporterActivity.kt
+++ app/src/main/java/pksokolowski/github/com/threegoals/reporter/ReporterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ScrollView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.reporter_activity.*
@@ -183,4 +184,16 @@ class ReporterActivity : AppCompatActivity() {
             goal_2 as ReportFormFragment,
             goal_3 as ReportFormFragment
     )
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        }
+    }
 }
